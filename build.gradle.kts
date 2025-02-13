1f06133e33c48bbdc4c28abe811b2517555f59c7
Xu Yiming <xuyiming.open@outlook.com>
2025-02-13 21:24:41 +0800

    build: Adjust for monorepo

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,20 +21,6 @@ tasks {
     }
 }
 
-repositories {
-    mavenCentral()
-    mavenLocal()
-    google()
-    maven {
-        // A repository must be speficied for some reason. "registry" is a dummy.
-        url = uri("https://maven.pkg.github.com/revanced/registry")
-        credentials {
-            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
-        }
-    }
-}
-
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.xpp3)
--- settings.gradle.kts
+++
@@ -1,7 +0,0 @@
-rootProject.name = "revanced-patcher"
-
-buildCache {
-    local {
-        isEnabled = "CI" !in System.getenv()
-    }
-}
